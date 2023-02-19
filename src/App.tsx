63b1d17146a6d129c94174008eadd7edced0912e
Shahar Yakir <shahar.yakir@gmail.com>
2023-02-19 16:32:06 +0200

    -

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { CHAIN } from "@tonconnect/protocol";
 import "@twa-dev/sdk";
-\
+
 const StyledApp = styled.div`
   background-color: #e8e8e8;
   color: black;
